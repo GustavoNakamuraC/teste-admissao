ce2f0caef98f22276702294bc43d9e521c8bde5f
GustavoNakamuraC <gustavonakamura6@gmail.com>
2025-04-10 16:29:02 -0300

    Pergunta sobre Orientação a Objeto

---
+++ orientado_a_objeto/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * Escreva a diferença entre interfaces, instancias, objetos e classes no contexto de orientação a objeto:
+ */
+
+/**
+ * Interfaces: Ela abrange apenas métodos formando meio que uma espécie de contrato quando usada, sendo aquela classe que a implementar tendo que usar todos os métodos dessa interface.
+ *
+ * Classes: É uma abstração de algo, podendo ter atributos como características e métodos representando as ações desta abstração.
+ *
+ * Instancia: Para inicializar um objeto, você precisará instânciá-lo primeiro. É no processo de instanciação que acontece a contrução do objeto.
+ *
+ * Objeto: Após instanciado, o objeto se torna a aplicação da classe, tendo os seus atributos definidos com valores e e seus métodos podendo ser acessado para fazer qualquer ação dentro do código.
+ */
+public class Main {
+    public static void main(String[] args) {
+    }
+}
